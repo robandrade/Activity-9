2a65cd5cb518915d5b309e79e4589027578fa93d
robandrade <103383943+robandrade@users.noreply.github.com>
2022-04-26 20:44:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Methods
+{
+    public static void showSum(int num1, int num2)
+    {
+        
+    }
+
+    public static double average(double num1, double num2, double num3, double num4, double num5)
+    {
+        
+    }
+
+    public static double randomSum()
+    {
+        
+    }
+
+    public static bool checkDivisible(int num1, int num2, int num3)
+    {
+
+    }
+
+    public static void displayString(string s1, string s2)
+    {
+
+    }
+
+    public static void largestValue(double[] arr)
+    {
+        
+    }
+
+    public static int[] fiftyValues()
+    {
+
+    }
+
+    public static bool trueorFalse(bool b1, bool b2)
+    {
+
+    }
+
+    public static void variableSum(int num1, double num2)
+    {
+
+    }
+
+    public static void averageEntries(int[,] arr)
+    {
+
+    }
+
+}
